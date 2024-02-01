e925e378b7dfc5f413646830d9873d7ae229a54b
rkdan <rkd43@cam.ac.uk>
2024-02-01 16:56:42 +0000

    Fixed keystone bug

--- keystone/keystone.py
+++ keystone/keystone.py
@@ -5,14 +5,24 @@ from tqdm import tqdm
 
 
 def calculate_keystone(j, sample, original_abundance, model):
+    # if the abundance is already zero, then the keystone index is zero
     p = original_abundance[j]
-    sample[j] = 0
-    original_abundance[j] = 0
 
-    null_abundance = original_abundance / original_abundance.sum()
+    if p == 0:
+        return 0
 
     with torch.no_grad():
-        predicted_abundance = model(torch.tensor(sample.reshape(1, -1), dtype=torch.float32)).numpy()
+        new_abundance = model(torch.tensor(sample.reshape(1, -1), dtype=torch.float32)).numpy().flatten()
+
+    new_sample = sample.copy()
+
+    new_sample[j] = 0
+    new_abundance[j] = 0
+
+    null_abundance = new_abundance / new_abundance.sum()
+
+    with torch.no_grad():
+        predicted_abundance = model(torch.tensor(new_sample.reshape(1, -1), dtype=torch.float32)).numpy()
     
     k = distance.braycurtis(null_abundance, predicted_abundance.flatten()) * (1 - p)
 
